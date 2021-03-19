e14e077aebd24afbc2af16fafbe22cb51eaea26e
Vladimir <vladgrom@yahoo.com>
2021-03-19 13:16:01 +0000

    Initial Commit

---
+++ SportNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SportNews
+//
+//  Created by Mac on 19/03/2021.
+//  Copyright © 2021 Mac.Vlad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
