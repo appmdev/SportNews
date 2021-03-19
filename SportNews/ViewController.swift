dcb6f6af9b3749afa3f38eb5c0728ecf937e2595
Vladimir <vladgrom@yahoo.com>
2021-03-19 13:40:21 +0000

    adds new print statement in viewController.swift

--- SportNews/ViewController.swift
+++ SportNews/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("Hello World")
+        print("Another print statement")
         // Do any additional setup after loading the view.
     }
 
